caf230c23769e241441e18f7e8b741d466c8f86b
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-11 16:07:48 +0800

    Update

--- src/functional_components/func_card.jsx
+++ src/functional_components/func_card.jsx
@@ -11,7 +11,11 @@ function Func_Card({image, title, text}){
             <img src={image} alt="" className="w-[500px] h-[350px] rounded-2xl"/>
             <h1 className="text-2xl">{title}</h1>
             <p className="text-[1rem]">{text}</p>
-            <button type="button" className="border-2 rounded-[10px] p-0.5 w-full" onClick={() => setHasVoted(true)}>
+            <button type="button" className="border-2 rounded-[10px] p-0.5 w-full cursor-pointer" 
+            onClick={() => {
+                setHasVoted(true)
+                console.log("log")
+            }}>
                 {hasVoted ? "Voted": "Vote"}
             </button>
         </div>
