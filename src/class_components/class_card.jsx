ad8cfb0dab30effcefb548d1063d353c20794a76
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-15 19:37:33 +0800

    Subject: Update

--- src/class_components/class_card.jsx
+++ src/class_components/class_card.jsx
@@ -1,27 +1,23 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
+class Class_Card extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-class Class_Card extends Component{
+  render() {
+    const {image, title, text} = this.props;
 
-    constructor(props){
-        super(props)
-
-    }
-
-    render(){
-
-        const [image, title, text] = this.props
-
-        return (
-            <div>
-                <img src={image} alt="" srcset="" />
-                <h1>{title}</h1>
-                <p>{text}</p>
-            </div>
-        )
-    }
-
-
+    return (
+      <>
+        <div>
+          <img src={image} alt="" srcset="" />
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </div>
+      </>
+    )
+  }
 }
 
 export default Class_Card;
