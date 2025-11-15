ec947274a087b6e9b509c8bb3c0d6e00b80776d6
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-15 19:15:54 +0800

    Subject: Update

---
+++ src/class_components/class_card.jsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+
+class Class_Card extends Component{
+
+    constructor({image, title, text}){
+        super()
+        this.image = image;
+        this.title = title;
+        this.text = text;
+    }
+
+    render(){
+        <div>
+            <img src={this.image} alt="" srcset="" />
+            <h1>{this.title}</h1>
+            <p>{this.text}</p>
+        </div>
+    }
+
+
+}
