a22c3051306d8544d56278edfe57e20e54a84e51
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-17 16:25:50 +0800

    Subject: Update

--- src/class_components/class_card.jsx
+++ src/class_components/class_card.jsx
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 class Class_Card extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {hasLiked: false} // state
   }
 
+  clickedLike = () => {
+    this.setState({hasLiked: !this.state.hasLiked})
+  }
   
 
   render() {
-    const {image, title, text} = this.props;
+    const {image, title, text} = this.props; // props
 
     return (
       <>
@@ -17,6 +20,9 @@ class Class_Card extends Component {
           <img src={image} alt="" srcset="" />
           <h1>{title}</h1>
           <p>{text}</p>
+          <button type="button" onClick={this.clickedLike}>
+            {this.state.hasLiked ? "Liked": "Like" } 
+          </button> 
         </div>
       </>
     )
