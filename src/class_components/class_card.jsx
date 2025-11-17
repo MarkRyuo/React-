06347ba8e8b4a6e36199d7c6a7c627ebfdf48c22
Riyuo <arcx@Riyuos-MacBook-Air.local>
2025-11-17 20:53:13 +0800

    Subject: Update

--- src/class_components/class_card.jsx
+++ src/class_components/class_card.jsx
@@ -16,11 +16,11 @@ class Class_Card extends Component {
 
     return (
       <>
-        <div>
-          <img src={image} alt="" srcset="" />
+        <div className="max-w-3xs min-w-[200px] p-1.5">
+          <img src={image} alt="" srcset="" className="rounded-2xl"/>
           <h1>{title}</h1>
           <p>{text}</p>
-          <button type="button" onClick={this.clickedLike}>
+          <button type="button" onClick={this.clickedLike} className="border-2 rounded-[10px] p-0.5 w-full cursor-pointer">
             {this.state.hasLiked ? "Liked": "Like" } 
           </button> 
         </div>
@@ -30,3 +30,8 @@ class Class_Card extends Component {
 }
 
 export default Class_Card;
+
+
+// Destructuring 
+// Ternary 
+// state 
